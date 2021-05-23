1909cfcfc95ba514e899c7dfd1505a443d763672
richeberry <65588098+richeberry@users.noreply.github.com>
2021-05-23 21:30:49 +0900

    Add files via upload

---
+++ cote_getaverage.py
@@ -0,0 +1,14 @@
+# 평균 구하기
+
+'''arr = [1, 2, 3, 4]
+print(len(arr))
+print(sum(arr))
+a = sum(arr) / len(arr)
+print(a)
+'''
+
+def solution(arr):
+    return sum(arr)/len(arr)
+
+
+print(solution([1,2,3,4]))
