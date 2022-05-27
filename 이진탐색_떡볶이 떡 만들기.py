bd982b8537704011ce8b5e6d8ab1c6157b27e33b
richeberry <richeberry@gmail.com>
2022-05-27 23:18:53 +0900

    Inflearn_3

--- 이진탐색_떡볶이 떡 만들기.py
+++ 이진탐색_떡볶이 떡 만들기.py
@@ -8,11 +8,11 @@ end = max(array)
 
 # 이진 탐색 수행(반복)
 result = 0
-while start <= end:
+while start <= end: # 자를 떡이 더 길다면
     total = 0
-    mid = (start + end) // 2
+    mid = (start + end) // 2 
     for x in array:
-        if x > mid:
+        if x > mid: # 중간 값보다 길면 늘이기
             total += x - mid
     
     if total < m:
