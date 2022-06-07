f2f62d79f7d07460fa73effd74aabd3b7e06b2e2
richeberry <richeberry@gmail.com>
2022-06-07 23:31:24 +0900

    이코테_그래프

--- 이진탐색_떡볶이 떡 만들기.py
+++ 이진탐색_떡볶이 떡 만들기.py
@@ -8,11 +8,11 @@ end = max(array)
 
 # 이진 탐색 수행(반복)
 result = 0
-while start <= end: # 자를 떡이 더 길다면
+while start <= end:
     total = 0
-    mid = (start + end) // 2 
+    mid = (start + end) // 2
     for x in array:
-        if x > mid: # 중간 값보다 길면 늘이기
+        if x > mid:
             total += x - mid
     
     if total < m:
