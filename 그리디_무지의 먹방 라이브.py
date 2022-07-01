aeb68410c52c4fc8de3ab227eec2620405dd50f5
richeberry <richeberry@gmail.com>
2022-07-01 23:35:30 +0900

    이코테_그리디

--- 그리디_무지의 먹방 라이브.py
+++ 그리디_무지의 먹방 라이브.py
@@ -1,22 +1,19 @@
 # 무지의 먹방 라이브
 
-food_times = [3, 1, 2]
-k = 5
 
-answer = 0
-quo = k // len(food_times)
-divided = k % len(food_times)
-print('quo, divided', quo, divided)
+def solution(food_times, k):
+    from collections import deque
+
+    data = deque()
+
+    for i in food_times:
+        data.append(i)
 
 
-for i in range(len(food_times)):
-    food_times[i] -= quo
+    for i in range(k):
+        data[0] -= 1
+        data.rotate(-1)
+        if data[0] == 0:
+            data.popleft()
 
-for i in food_times:
-    if i == 0:
-        food_times.pop(food_times[i-1])
-
-print(food_times)
-
-for i in range(divided):
-    print(i)
+    return data[0]
