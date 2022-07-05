a1554618e708ff83a31a11d43fa08129af664ade
richeberry <richeberry@gmail.com>
2022-07-05 23:29:48 +0900

    이코테_구현

--- 구현_문자열 재정렬.py
+++ 구현_문자열 재정렬.py
@@ -1,5 +1,8 @@
 # 이코테 _ 구현 _ 문자열 재정렬
 
+
+# 첫 번째 풀이
+'''
 s = input()
 
 alpha = []
@@ -13,3 +16,15 @@ for i in s:
 
 alpha.sort()
 print(f'{"".join(alpha)}{num}')
+'''
+
+# 두 번째 풀이
+
+import re
+
+s = input()
+num = 0
+for i in re.findall('\d',s):
+    num += int(i)
+s = ''.join(sorted(re.findall('\D',s)))
+print(f'{s}{num}')
