a63f78689d6e761665ef14caaca523b4bb6d9771
richeberry <richeberry@gmail.com>
2022-07-08 23:54:03 +0900

    이코테_구현

---
+++ 구현_뱀.py
@@ -0,0 +1,35 @@
+# 이코테 _ 구현 _ 뱀
+
+n = 6
+
+# game = [[0] * n for _ in range(n)]
+# print(game)
+
+# k = int(input())
+k = 3
+
+# 사과 표시 _ game에 사과 위치를 1로 표시
+# for _ in range(k):
+#     a, b = map(int, input().split())
+#     game[a-1][b-1] = 1
+
+# 사과가 있는 게임
+game = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0], [0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+head = game[0][0] # 뱀이 처음 있을 위치
+
+# l = int(input())
+l = 3
+# direct = [] # 회전 방향을 담을 리스트
+# for _ in range(l):
+#     a, b = map(str, input().split())
+#     direct.append((int(a), b))
+
+direct = [(3, 'D'), (15, 'L'), (17, 'D')]
+
+# 게임이 몇 초에 끝나는지 출력
+
+tail = 0
+cnt = 0
+while head == tail:
+    for i in direct:
+        print(i)
