aa8e69cf6d2f3b99145737fe1c9c073780f24dee
richeberry <richeberry@gmail.com>
2022-07-09 23:52:35 +0900

    이코테_뱀

--- 구현_뱀.py
+++ 구현_뱀.py
@@ -2,7 +2,7 @@
 
 n = 6
 
-# game = [[0] * n for _ in range(n)]
+game = [[0] * n for _ in range(n)]
 # print(game)
 
 # k = int(input())
@@ -11,12 +11,15 @@ k = 3
 # 사과 표시 _ game에 사과 위치를 1로 표시
 # for _ in range(k):
 #     a, b = map(int, input().split())
-#     game[a-1][b-1] = 1
+#     game[a-1][b-1] = 'apple'
+
 
 # 사과가 있는 게임
-game = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0], [0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+game = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 'apple', 0], [0, 0, 0, 'apple', 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 'apple', 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+game[0][0] = 1
 head = game[0][0] # 뱀이 처음 있을 위치
 
+print(game)
 # l = int(input())
 l = 3
 # direct = [] # 회전 방향을 담을 리스트
@@ -30,6 +33,7 @@ direct = [(3, 'D'), (15, 'L'), (17, 'D')]
 
 tail = 0
 cnt = 0
-while head == tail:
-    for i in direct:
-        print(i)
+now = 0
+while head != tail:
+    for i in range(direct[now][0]):
+        if direct[now][1] == 'D':
