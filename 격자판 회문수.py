3ea1f48568eacd4b9dc639afb3b1084d5390845e
richeberry <richeberry@gmail.com>
2023-01-06 17:32:10 +0900

    sdoku

---
+++ 격자판 회문수.py
@@ -0,0 +1,17 @@
+# Inflearn _ Section 3 _ 격자판 회문수
+
+import sys
+board = [list(map(int, input().split())) for _ in range(7)]
+cnt = 0
+for i in range(3):
+    for j in range(7):
+        tmp = board[j][i:i+5]
+        if tmp == tmp[::-1]:
+            cnt += 1
+        for k in range(2):
+            if board[i+k][j]!=board[i+5-k-1][j]:
+                break
+        else:
+            cnt += 1
+
+print(cnt)
